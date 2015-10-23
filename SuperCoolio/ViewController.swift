7ce014595af9de34147374480c37caf5641207b3
Darvin C Melton <Darv@darvs-macbook-pro.local>
2015-10-22 23:34:05 -0700

    Initial Commit

---
+++ SuperCoolio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperCoolio
+//
+//  Created by Darvin C Melton on 2015-10-22.
+//  Copyright © 2015 Roger Moeller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
